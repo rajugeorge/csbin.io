ea7b0352e74f7eb773bf6758c4639182234b3f4b
Raju George <raju.george@yahoo.com>
2021-08-19 11:53:05 +0530

    Promises added

---
+++ promises.js
@@ -0,0 +1,72 @@
+function sayHello() {
+  setTimeout(() => {
+    console.log("Hello!");
+  }, 3000);
+}
+
+function promise() {
+  return new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      resolve("Resolved!!!");
+    }, 1000);
+  });
+}
+// const promise = new Promise(function (resolve, reject) {
+//   setTimeout(() => {
+//     resolve("Resolved!!!");
+//   }, 1000);
+// });
+
+function promise2() {
+  return new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      reject("Rejected!!!");
+    }, 1000);
+  });
+}
+
+function delay() {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, 1000);
+  });
+}
+
+function secondPromise() {
+  return Promise.resolve("Second!!!");
+}
+
+function firstPromise(promise) {
+  return Promise.resolve(promise);
+}
+
+const fakePeople = [
+  { name: "Rudolph", hasPets: false, currentTemp: 98.6 },
+  { name: "Zebulon", hasPets: true, currentTemp: 22.6 },
+  { name: "Harold", hasPets: true, currentTemp: 98.3 },
+];
+
+const fakeAPICall = (i) => {
+  const returnTime = Math.floor(Math.random() * 1000);
+  return new Promise((resolve, reject) => {
+    if (i >= 0 && i < fakePeople.length) {
+      setTimeout(() => resolve(fakePeople[i]), returnTime);
+    } else {
+      reject({ message: "index out of range" });
+    }
+  });
+};
+
+function getAllData() {
+  // CODE GOES HERE
+  return Promise.all([fakeAPICall(0), fakeAPICall(1), fakeAPICall(2)]);
+}
+
+module.exports = {
+  sayHello,
+  promise,
+  promise2,
+  delay,
+  secondPromise,
+  firstPromise,
+  getAllData,
+};
--- promises.test.js
+++ promises.test.js
@@ -1,1 +1,73 @@
-test.todo("TBD");
+// uncomment below line if you are returning the promise commented for second challenge in promises.js
+//jest.useFakeTimers();
+const promises = require("./promises");
+
+afterEach(() => {
+  jest.useRealTimers();
+});
+
+test("sayHello", () => {
+  //   promises.sayHello = () => "Hello!";
+
+  jest.useFakeTimers();
+  promises.sayHello();
+  jest.runAllTimers();
+});
+
+test("promise", async () => {
+  // promises.promise = Promise.resolve("Resolved!!");
+  jest.useFakeTimers();
+  const spy = jest.fn((data) => {
+    expect(data).toEqual("Resolved!!!");
+  });
+  promises.promise().then(spy);
+  jest.runAllTimers();
+  await Promise.resolve(); // execute the promises in the Promises Job Queue
+  expect(spy).toHaveBeenCalled();
+});
+
+test("promise2", async () => {
+  jest.useFakeTimers();
+  const spy = jest.fn((data) => {
+    expect(data).toEqual("Rejected!!!");
+  });
+  promises.promise2().catch(spy);
+  jest.runAllTimers();
+  await Promise.resolve(); // execute the promises in the Promises Job Queue
+  expect(spy).toHaveBeenCalled();
+});
+
+// TBD
+test("delay", async () => {
+  // promises.delay = () => Promise.resolve();
+  jest.useFakeTimers();
+  const sayHello = jest.fn(promises.sayHello);
+  promises.delay().then(sayHello);
+  jest.runAllTimers();
+  await Promise.resolve();
+  expect(sayHello).toHaveBeenCalled();
+});
+
+test("promiseChain", async () => {
+  // promises.secondPromise = Promise.resolve("Second!!!");
+  // promises.firstPromise = Promise.resolve(promises.secondPromise);
+
+  const secondPromise = promises.secondPromise();
+  const firstPromise = promises.firstPromise(secondPromise);
+
+  firstPromise.then().then((data) => {
+    expect(data).toEqual("Second!!!");
+  });
+
+  await Promise.resolve();
+});
+
+test("promiseAll", async () => {
+  promises.getAllData().then((values) => {
+    expect(values).toEqual([
+      { currentTemp: 98.6, hasPets: false, name: "Rudolph" },
+      { currentTemp: 22.6, hasPets: true, name: "Zebulon" },
+      { currentTemp: 98.3, hasPets: true, name: "Harold" },
+    ]);
+  });
+});
