18f2f922261250bb5d7bafc2fe476359bf83da5f
Raju George <raju.george@yahoo.com>
2021-08-24 09:04:19 +0530

    oops completed

--- oops.js
+++ oops.js
@@ -7,11 +7,117 @@ function makePerson(name, age) {
 
 const personStore = {
   greet: function () {
-    console.log("hello");
+    return "hello";
+  },
+};
+
+function personFromPersonStore(name, age) {
+  // add code here
+  const obj = Object.create(personStore);
+  obj.name = name;
+  obj.age = age;
+  return obj;
+}
+
+const sandra = personFromPersonStore("Sandra", 26);
+
+personStore.introduce = function (name) {
+  return `Hi, my name is ${name}`;
+};
+
+function PersonConstructor() {
+  // add code here
+  this.greet = function () {
+    return "hello";
+  };
+}
+
+function personFromConstructor(name, age) {
+  const person = new PersonConstructor();
+  person.name = name;
+  person.age = age;
+  return person;
+}
+
+PersonConstructor.prototype.introduce = function () {
+  return `Hi, my name is ${this.name}`;
+};
+
+class PersonClass {
+  constructor(name) {
+    // add code here
+    this.name = name;
+  }
+
+  greet() {
+    return "hello";
+  }
+}
+
+class DeveloperClass extends PersonClass {
+  constructor(name, age) {
+    super(name);
+    this.age = age;
+  }
+  introduce() {
+    return `Hello World, my name is ${this.name}`;
+  }
+}
+
+const userFunctionStore = {
+  sayType: function () {
+    return `I am a ${this.type}`;
+  },
+};
+
+function userFactory(name, score) {
+  let user = Object.create(userFunctionStore);
+  user.type = "User";
+  user.name = name;
+  user.score = score;
+  return user;
+}
+
+const adminFunctionStore = Object.create(userFunctionStore);
+
+adminFunctionStore.sharedPublicMessage = function (text) {
+  return text;
+};
+
+function adminFactory(name, score) {
+  const user = userFactory(name, score);
+  Object.setPrototypeOf(user, adminFunctionStore);
+  user.type = "Admin";
+  return user;
+}
+
+class Dog {
+  constructor() {
+    this.legs = 4;
+  }
+  speak() {
+    return "Woof!";
+  }
+}
+
+const robotMixin = {
+  skin: "metal",
+  speak: function () {
+    return `I have ${this.legs} legs and am made of ${this.skin}`;
   },
 };
 
 module.exports = {
   makePerson,
   personStore,
+  personFromPersonStore,
+  PersonConstructor,
+  personFromConstructor,
+  PersonClass,
+  DeveloperClass,
+  adminFunctionStore,
+  userFactory,
+  adminFactory,
+  Dog,
+  robotMixin,
 };
--- oops.test.js
+++ oops.test.js
@@ -9,44 +9,105 @@ test("makePerson", () => {
 
   expect(result).toEqual(expected);
 });
+
 test("personStore", () => {
   //   oops.personStore = {
   //     greet: () => {
   //       console.log("hello");
   //     },
   //   };
-  oops.personStore.greet();
+  expect(oops.personStore.greet()).toEqual("hello");
 });
+
 test("personFromPersonStore", () => {
   // oops.personFromPersonStore = (name, age) => {
   //   return {
   //     name: name,
   //     age: age,
   //     greet: () => {
-  //       console.log("hello");
+  //       return "hello";
   //     },
   //   };
   // };
-  // const result = oops.personFromPersonStore("Tim", 22);
-  // const expected = {
-  //   name: "Tim",
-  //   age: 22,
-  //   greet: () => {
-  //     console.log("hello");
-  //   },
-  // };
-  // console.log(result);
-  // expect(result).toEqual(expected);
+  const result = oops.personFromPersonStore("Tim", 22);
+
+  expect(result.name).toEqual("Tim"); // -> Logs 'Sandra'
+  expect(result.age).toEqual(22); //-> Logs 26
+  expect(result.greet()).toEqual("hello");
 });
-test.todo("personFromPersonStoreintroduce");
-test.todo("PersonConstructor");
-test.todo("personFromConstructor");
-test.todo("personFromConstructorintroduce");
-test.todo("PersonClass");
-test.todo("DeveloperClass");
-test.todo("adminFunctionStore");
-test.todo("adminFactory");
-test.todo("adminFactory2");
-test.todo("adminFactory3");
-test.todo("adminFromFactory");
-test.todo("Mixins");
+
+test("personFromPersonStoreintroduce", () => {
+  const result = oops.personStore.introduce("Sandra");
+  expect(result).toEqual("Hi, my name is Sandra");
+});
+
+test("PersonConstructor", () => {
+  const simon = new oops.PersonConstructor();
+  const result = simon.greet();
+
+  expect(result).toEqual("hello");
+});
+
+test("personFromConstructor", () => {
+  const mike = oops.personFromConstructor("Mike", 30);
+
+  expect(mike.name).toEqual("Mike"); // -> Logs 'Mike'
+  expect(mike.age).toEqual(30); //-> Logs 30
+  expect(mike.greet()).toEqual("hello"); //-> Logs 'hello'
+});
+
+test("personFromConstructorintroduce", () => {
+  const mike = oops.personFromConstructor("Mike", 30);
+  expect(mike.introduce()).toEqual("Hi, my name is Mike");
+});
+
+test("PersonClass", () => {
+  // oops.PersonClass = class PersonClass {
+  //   greet() {
+  //     return "hello";
+  //   }
+  // };
+
+  const george = new oops.PersonClass();
+  expect(george.greet()).toEqual("hello");
+});
+
+test("DeveloperClass", () => {
+  // oops.DeveloperClass = class DeveloperClass {};
+  const thai = new oops.DeveloperClass("Thai", 32);
+  expect(thai.name).toEqual("Thai"); // -> Logs 'Thai'
+  expect(thai.introduce()).toEqual("Hello World, my name is Thai");
+});
+test("adminFunctionStore", () => {
+  const admin = oops.adminFunctionStore;
+  admin.type = "Humanoid";
+  const result = admin.sayType();
+
+  expect(result).toEqual("I am a Humanoid");
+});
+
+test("adminFactory", () => {
+  const adminFactory = oops.adminFactory("Eva", 5);
+
+  expect(adminFactory.sayType()).toEqual("I am a Admin");
+});
+
+test("adminFromFactory", () => {
+  const adminFromFactory = oops.adminFactory("Eva", 5);
+
+  expect(adminFromFactory.sayType()).toEqual("I am a Admin");
+  expect(adminFromFactory.sharedPublicMessage("Welcome users!")).toEqual(
+    "Welcome users!"
+  );
+});
+
+test("Mixins", () => {
+  const Dog = oops.Dog;
+  const robotMixin = oops.robotMixin;
+
+  let robotFido = new oops.Dog();
+
+  Object.assign(robotFido, robotMixin);
+  const result = robotFido.speak();
+  expect(result).toEqual("I have 4 legs and am made of metal");
+});
